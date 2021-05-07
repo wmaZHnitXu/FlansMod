b8825690fa791437b8d905e60c11b9bc6b9026d9
wmaZHnitXu <alesternest@gmail.com>
2021-05-07 15:57:37 +0300

    spread

--- src/main/java/com/flansmod/client/model/mw/ModelSentryMg.java
+++ src/main/java/com/flansmod/client/model/mw/ModelSentryMg.java
@@ -21,9 +21,6 @@ public class ModelSentryMg extends ModelAAGun //Same as Filename
 
 	public ModelSentryMg() //Same as Filename
 	{
-		textureX = 512;
-		textureY = 512;
-
 		baseModel = new ModelRendererTurbo[10];
 		baseModel[0] = new ModelRendererTurbo(this, 329, 75, textureX, textureY); // Box 0
 		baseModel[1] = new ModelRendererTurbo(this, 65, 1, textureX, textureY); // Box 1
@@ -37,40 +34,40 @@ public class ModelSentryMg extends ModelAAGun //Same as Filename
 		baseModel[9] = new ModelRendererTurbo(this, 385, 1, textureX, textureY); // Box 10
 
 		baseModel[0].addBox(0F, 0F, 0F, 14, 3, 14, 0F); // Box 0
-		baseModel[0].setRotationPoint(-7F, 1F, -7F);
+		baseModel[0].setRotationPoint(-7F, -2F, -7F);
 
 		baseModel[1].addBox(0F, 0F, 0F, 12, 2, 12, 0F); // Box 1
-		baseModel[1].setRotationPoint(-6F, 2F, 7F);
+		baseModel[1].setRotationPoint(-6F, -1F, 7F);
 
 		baseModel[2].addBox(0F, 0F, 0F, 12, 2, 12, 0F); // Box 3
-		baseModel[2].setRotationPoint(-6F, 2F, -19F);
+		baseModel[2].setRotationPoint(-6F, -1F, -19F);
 
 		baseModel[3].addBox(0F, 0F, 0F, 12, 2, 12, 0F); // Box 4
-		baseModel[3].setRotationPoint(7F, 2F, 6F);
+		baseModel[3].setRotationPoint(7F, -1F, 6F);
 		baseModel[3].rotateAngleY = -1.57079633F;
 
 		baseModel[4].addBox(0F, 0F, 0F, 12, 2, 12, 0F); // Box 5
-		baseModel[4].setRotationPoint(-19F, 2F, 6F);
+		baseModel[4].setRotationPoint(-19F, -1F, 6F);
 		baseModel[4].rotateAngleY = -1.57079633F;
 
 		baseModel[5].addShape3D(10F, 0F, -9F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(10, 0, 10, 0), new Coord2D(10, 2, 10, 2) }), 18, 10, 2, 23, 18, ModelRendererTurbo.MR_FRONT, new float[] {11 ,2 ,10}); // Shape 6
-		baseModel[5].setRotationPoint(5F, 4.1F, -5F);
+		baseModel[5].setRotationPoint(5F, 1.1F, -5F);
 		baseModel[5].rotateAngleY = -0.78539816F;
 
 		baseModel[6].addShape3D(10F, 0F, -9F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(10, 0, 10, 0), new Coord2D(10, 2, 10, 2) }), 18, 10, 2, 23, 18, ModelRendererTurbo.MR_FRONT, new float[] {11 ,2 ,10}); // Shape 7
-		baseModel[6].setRotationPoint(-5F, 4.1F, 5F);
+		baseModel[6].setRotationPoint(-5F, 1.1F, 5F);
 		baseModel[6].rotateAngleY = -3.92699082F;
 
 		baseModel[7].addShape3D(10F, 0F, -9F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(10, 0, 10, 0), new Coord2D(10, 2, 10, 2) }), 18, 10, 2, 23, 18, ModelRendererTurbo.MR_FRONT, new float[] {11 ,2 ,10}); // Shape 8
-		baseModel[7].setRotationPoint(5F, 4.1F, 5F);
+		baseModel[7].setRotationPoint(5F, 1.1F, 5F);
 		baseModel[7].rotateAngleY = 0.78539816F;
 
 		baseModel[8].addShape3D(10F, 0F, -9F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(10, 0, 10, 0), new Coord2D(10, 2, 10, 2) }), 18, 10, 2, 23, 18, ModelRendererTurbo.MR_FRONT, new float[] {11 ,2 ,10}); // Shape 9
-		baseModel[8].setRotationPoint(-5F, 4.1F, -5F);
+		baseModel[8].setRotationPoint(-5F, 1.1F, -5F);
 		baseModel[8].rotateAngleY = -2.35619449F;
 
 		baseModel[9].addBox(0F, 0F, 0F, 12, 4, 12, 0F); // Box 10
-		baseModel[9].setRotationPoint(-6F, -3F, -6F);
+		baseModel[9].setRotationPoint(-6F, -6F, -6F);
 
 
 		seatModel = new ModelRendererTurbo[12];
@@ -88,44 +85,44 @@ public class ModelSentryMg extends ModelAAGun //Same as Filename
 		seatModel[11] = new ModelRendererTurbo(this, 19, 173, textureX, textureY); // Shape 27
 
 		seatModel[0].addBox(0F, 0F, 0F, 23, 1, 17, 0F); // Box 12
-		seatModel[0].setRotationPoint(-16F, -4F, -8.5F);
+		seatModel[0].setRotationPoint(-16F, -7F, -8.5F);
 
 		seatModel[1].addBox(0F, 0F, 0F, 26, 6, 17, 0F); // Box 13
-		seatModel[1].setRotationPoint(-19F, -10F, -8.5F);
+		seatModel[1].setRotationPoint(-19F, -13F, -8.5F);
 
 		seatModel[2].addShape3D(0F, 0F, 0F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(3, 0, 3, 0), new Coord2D(3, 6, 3, 6) }), 17, 3, 8, 16, 17, ModelRendererTurbo.MR_FRONT, new float[] {7 ,6 ,3}); // Shape 14
-		seatModel[2].setRotationPoint(10F, -10F, -8.5F);
+		seatModel[2].setRotationPoint(10F, -13F, -8.5F);
 		seatModel[2].rotateAngleX = 3.14159265F;
 
 		seatModel[3].addBox(0F, 0F, 0F, 22, 11, 17, 0F); // Box 15
-		seatModel[3].setRotationPoint(-28F, -21F, -8.5F);
+		seatModel[3].setRotationPoint(-28F, -24F, -8.5F);
 
 		seatModel[4].addShape3D(0F, 0F, 0F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(16, 0, 16, 0), new Coord2D(16, 11, 16, 11) }), 3, 16, 11, 47, 3, ModelRendererTurbo.MR_FRONT, new float[] {20 ,11 ,16}); // Shape 16
-		seatModel[4].setRotationPoint(10F, -10F, 8.5F);
+		seatModel[4].setRotationPoint(10F, -13F, 8.5F);
 
 		seatModel[5].addShape3D(0F, 0F, 0F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(16, 0, 16, 0), new Coord2D(16, 11, 16, 11) }), 3, 16, 11, 47, 3, ModelRendererTurbo.MR_FRONT, new float[] {20 ,11 ,16}); // Shape 17
-		seatModel[5].setRotationPoint(10F, -10F, -5.5F);
+		seatModel[5].setRotationPoint(10F, -13F, -5.5F);
 
 		seatModel[6].addShape3D(0F, 0F, 0F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(16, 0, 16, 0), new Coord2D(16, 11, 16, 11) }), 3, 16, 11, 47, 3, ModelRendererTurbo.MR_FRONT, new float[] {20 ,11 ,16}); // Shape 18
-		seatModel[6].setRotationPoint(10F, -10F, 1.5F);
+		seatModel[6].setRotationPoint(10F, -13F, 1.5F);
 
 		seatModel[7].addShape3D(0F, 0F, 0F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(6, 0, 6, 0), new Coord2D(6, 9, 6, 9) }), 17, 6, 9, 26, 17, ModelRendererTurbo.MR_FRONT, new float[] {11 ,9 ,6}); // Shape 19
-		seatModel[7].setRotationPoint(-19F, -4F, -8.5F);
+		seatModel[7].setRotationPoint(-19F, -7F, -8.5F);
 		seatModel[7].rotateAngleX = 3.14159265F;
 		seatModel[7].rotateAngleZ = 4.71238898F;
 
 		seatModel[8].addBox(0F, 0F, 0F, 12, 4, 5, 0F); // Box 18
-		seatModel[8].setRotationPoint(-28F, -21F, -13.5F);
+		seatModel[8].setRotationPoint(-28F, -24F, -13.5F);
 
 		seatModel[9].addBox(0F, 0F, 0F, 18, 2, 12, 0F); // Box 19
-		seatModel[9].setRotationPoint(-28F, -23F, -8.5F);
+		seatModel[9].setRotationPoint(-28F, -26F, -8.5F);
 
 		seatModel[10].addShape3D(0F, 0F, 0F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(7, 0, 7, 0), new Coord2D(7, 5, 7, 5) }), 11, 7, 5, 21, 11, ModelRendererTurbo.MR_FRONT, new float[] {9 ,5 ,7}); // Shape 26
-		seatModel[10].setRotationPoint(1F, -16F, -8.5F);
+		seatModel[10].setRotationPoint(1F, -19F, -8.5F);
 		seatModel[10].rotateAngleX = 1.57079633F;
 
 		seatModel[11].addShape3D(0F, 0F, 0F, new Shape2D(new Coord2D[] { new Coord2D(0, 0, 0, 0), new Coord2D(7, 0, 7, 0), new Coord2D(7, 5, 7, 5) }), 11, 7, 5, 21, 11, ModelRendererTurbo.MR_FRONT, new float[] {9 ,5 ,7}); // Shape 27
-		seatModel[11].setRotationPoint(-13F, -5F, -8.5F);
+		seatModel[11].setRotationPoint(-13F, -8F, -8.5F);
 		seatModel[11].rotateAngleX = 1.57079633F;
 		seatModel[11].rotateAngleZ = 3.14159265F;
 
@@ -136,16 +133,16 @@ public class ModelSentryMg extends ModelAAGun //Same as Filename
 		gunModel[2] = new ModelRendererTurbo(this, 28, 219, textureX, textureY); // Box 22
 		gunModel[3] = new ModelRendererTurbo(this, 219, 78, textureX, textureY); // Box 23
 
-		gunModel[0].addBox(-7F, -16F, 2.5F, 47, 2, 2, 0F); // Box 20
+		gunModel[0].addBox(-7F, -20F, 2.5F, 47, 2, 2, 0F); // Box 20
 		gunModel[0].setRotationPoint(0F, 0F, 0F);
 
-		gunModel[1].addBox(-7F, -16F, -4.5F, 47, 2, 2, 0F); // Box 21
+		gunModel[1].addBox(-7F, -20F, -4.5F, 47, 2, 2, 0F); // Box 21
 		gunModel[1].setRotationPoint(0F, 0F, 0F);
 
-		gunModel[2].addBox(40F, -16.5F, -5F, 15, 3, 3, 0F); // Box 22
+		gunModel[2].addBox(40F, -20.5F, -5F, 15, 3, 3, 0F); // Box 22
 		gunModel[2].setRotationPoint(0F, 0F, 0F);
 
-		gunModel[3].addBox(40F, -16.5F, 2F, 15, 3, 3, 0F); // Box 23
+		gunModel[3].addBox(40F, -20.5F, 2F, 15, 3, 3, 0F); // Box 23
 		gunModel[3].setRotationPoint(0F, 0F, 0F);
 
 
--- src/main/java/com/flansmod/common/guns/EntityAAGun.java
+++ src/main/java/com/flansmod/common/guns/EntityAAGun.java
@@ -277,7 +277,7 @@ public class EntityAAGun extends Entity implements IEntityAdditionalSpawnData
 			if(target != null)
 			{
 				double dX = target.posX - posX;
-				double dY = target.posY - (posY + 1.5F);
+				double dY = target.posY - (posY + 0.5F);
 				double dZ = target.posZ - posZ;
 				
 				double distanceToTarget = Math.sqrt(dX * dX + dY * dY + dZ * dZ);
@@ -366,7 +366,6 @@ public class EntityAAGun extends Entity implements IEntityAdditionalSpawnData
 		
 		if(!world.isRemote && reloadTimer <= 0 && shootDelay <= 0)
 		{
-			FlansMod.log.info("bas1");
 			Boolean shootPlayer = mouseHeld && getControllingPassenger() instanceof EntityPlayerMP;
 			
 			if (target != null || shootPlayer)
@@ -377,7 +376,6 @@ public class EntityAAGun extends Entity implements IEntityAdditionalSpawnData
 				{
 					if(shootDelay <= 0 && ammo != null && !ammo.isEmpty() && (!type.fireAlternately || type.fireAlternately && currentBarrel == j))
 					{
-						FlansMod.log.info("bas3");
 						// Fire
 						BulletType bullet = BulletType.getBullet(ammo.getItem());
 						if(shootPlayer)
@@ -397,27 +395,37 @@ public class EntityAAGun extends Entity implements IEntityAdditionalSpawnData
 						Double radianYaw = Math.toRadians(gunYaw + 90F);
 						Double radianPitch = Math.toRadians(gunPitch);
 						Vector3f shootingDirection = new Vector3f(-Math.sin(radianYaw), Math.cos(radianYaw)*-Math.sin(radianPitch), Math.cos(radianYaw)*Math.cos(radianPitch));
-						FlansMod.log.info("bas5");
 						FireableGun weapon = new FireableGun(type, (float)type.damage, (float)type.accuracy, (float)type.damage);
 						FiredShot shot = new FiredShot(weapon, bullet, this, player);
 						//TODO use Vec3d
-						FlansMod.log.info("bas6");
 						ShotHandler.fireGun(world, shot, bullet.numBullets, new Vector3f(origin), shootingDirection);
-						FlansMod.log.info("bas7");
 						PacketPlaySound.sendSoundPacket(posX, posY, posZ, 50, dimension, type.shootSound, true);
-						FlansMod.log.info("bas8");
 					}
 				}
 				currentBarrel = (currentBarrel + 1) % type.numBarrels;
-				FlansMod.log.info("bas9");
 			}
 		}
 		if(!world.isRemote)
 		{
+			if (target != null && ticksExisted % 20 == 0 && !TargetTrace(target)) {
+				target = null;
+			}
 			FlansMod.getPacketHandler().sendToAllAround(new PacketAAGunAngles(this), posX, posY, posZ, 100F, dimension);
 			status = getStatus();
 			FlansMod.getPacketHandler().sendToAllAround(new PacketAAGunStatus(this), posX, posY, posZ, 100F, dimension);
 		}
+	}
+
+	public boolean TargetTrace (Entity e) {
+		if (e == null) return false;
+		Vec3d targetV = e.getPositionVector();
+		Vec3d step = e.getPositionVector().subtract(getPositionVector().add(0,1,0)).normalize();
+		Vec3d currentPos = getPositionVector().add(0,1,0);
+		while (currentPos.distanceTo(targetV) > 2) {
+			currentPos = currentPos.add(step);
+			if (world.getBlockState(new BlockPos((int)currentPos.x,(int)currentPos.y,(int)currentPos.z)).isNormalCube()) return false;
+		}
+		return true;
 	}
 	
 	public boolean isSentry()
@@ -458,7 +466,8 @@ public class EntityAAGun extends Entity implements IEntityAdditionalSpawnData
 								continue;
 						}
 					}
-					return candidateEntity;
+					if (TargetTrace(candidateEntity))
+						return candidateEntity;
 				}
 			}
 		}
